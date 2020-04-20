bb70badb8d8eab0c60e08ded908e3325fff20cb1
Kim Jong IL <jngkim@amazon.com>
2020-04-20 20:04:35 +0900

    Booking Result format change to json

--- src/main/java/com/proserve/kal/reservation/application/ReservationService.java
+++ src/main/java/com/proserve/kal/reservation/application/ReservationService.java
@@ -47,17 +47,17 @@ public class ReservationService {
 
         request.withConnectionId(requestInfo.getConnectionId());
 
-        request.withData(ByteBuffer.wrap("Receive Booking Request".getBytes()));
+        request.withData(ByteBuffer.wrap("{\"message\":\"Receive Booking Request\"}".getBytes()));
         agma.postToConnection(request);
 
         Thread.sleep(10000);
 
-        request.withData(ByteBuffer.wrap("Request to Booking System".getBytes()));
+        request.withData(ByteBuffer.wrap("{\"message\":\"Request to Booking System\"}".getBytes()));
         agma.postToConnection(request);
 
         Thread.sleep(10000);
 
-        request.withData(ByteBuffer.wrap("Finished Booking".getBytes()));
+        request.withData(ByteBuffer.wrap("{\"message\":\"Finished Booking\"}".getBytes()));
         agma.postToConnection(request);
 
     }
